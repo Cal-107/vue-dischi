143389afa7312726ea3beba032d2082c58f58689
“Cal-107” <kaly___7@hotmail.it>
2021-11-25 13:39:12 +0100

    add setup and header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+      <img src="@/assets/logo.png" alt="Logo Spotify">
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables.scss';
+
+.header {
+    background-color: $bg-1;
+}
+
+img {
+    width: 60px;
+    margin: .5rem;
+}
+</style>
