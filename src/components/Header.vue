44939038a5e66b65d7cfc068cb5f3a0d64d79d90
“Cal-107” <kaly___7@hotmail.it>
2021-11-26 15:24:20 +0100

    add select

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="header">
       <img src="@/assets/logo.png" alt="Logo Spotify">
+      <div class="choose-genre">
+        <label>Choose your genre</label>
+        <select>
+            <option value=''>All</option>
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="jazz">Jazz</option>
+            <option value="metal">Metal</option>
+        </select>
+      </div>
   </div>
 </template>
 
@@ -16,10 +26,33 @@ export default {
 
 .header {
     background-color: $bg-2;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: .5rem 1rem;
+    text-transform: uppercase;
+    .choose-genre {
+        display: flex;
+        align-items: center;
+        label {
+            margin-right: 1rem;
+        }
+        select {
+            color: $text-color-1;
+            background-color: #aaa;
+            padding: .4rem;
+            border: none;
+            border-radius: 30px;
+            option {
+                border: none;
+                background-color: #aaa;
+                border-radius: 20px;
+            }
+        }
+    }
 }
 
 img {
     width: 60px;
-    margin: .5rem;
 }
 </style>
